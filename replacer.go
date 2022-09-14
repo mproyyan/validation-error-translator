0696b812cf0bf28856e2c2a16d35bb9ba42b5291
royyan <mproyyan@gmail.com>
2022-09-14 09:06:07 +0700

    create replacer function to replace raw translation into actual translation

---
+++ replacer.go
@@ -0,0 +1,70 @@
+package tl
+
+import "github.com/go-playground/validator/v10"
+
+type replacerFunc func(raw string, f validator.FieldError) string
+
+type replacer struct {
+	tl      string
+	replace replacerFunc
+	next    *replacer
+}
+
+type replacerList struct {
+	head *replacer
+	tail *replacer
+}
+
+func newReplacer(rf replacerFunc) *replacer {
+	return &replacer{
+		replace: rf,
+	}
+}
+
+func newReplacerList() *replacerList {
+	return &replacerList{}
+}
+
+func (r *replacer) reset() {
+	r.tl = ""
+}
+
+func (rl *replacerList) Add(r *replacer) {
+	if rl.head == nil {
+		rl.head = r
+		rl.tail = r
+	} else {
+		rl.tail.next = r
+		rl.tail = r
+	}
+}
+
+func (rl *replacerList) replace(raw string, f validator.FieldError) string {
+	var translation string
+	list := rl.head
+
+	for list != nil {
+		if list.tl == "" {
+			list.tl = raw
+		}
+
+		if list.next != nil {
+			list.next.tl = list.replace(list.tl, f)
+
+			// reset current translation before moving to next replacer
+			list.reset()
+
+			list = list.next
+		} else {
+			list.tl = list.replace(list.tl, f)
+			translation = list.tl
+
+			// reset current translation before moving to next replacer
+			list.reset()
+
+			list = list.next
+		}
+	}
+
+	return translation
+}
---
+++ replacer_func.go
@@ -0,0 +1,45 @@
+package tl
+
+import (
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var replacers = []*replacer{
+	newReplacer(replaceFieldPlaceholder),
+	newReplacer(replaceParamPlaceholder),
+	newReplacer(replaceTagPlaceholder),
+	newReplacer(replaceActualTagPlaceholder),
+	newReplacer(replaceStructNamespacePlaceholder),
+	newReplacer(replaceStructFieldPlaceholder),
+	newReplacer(replaceNamespacePlaceholder),
+}
+
+func replaceFieldPlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":f", f.Field(), -1)
+}
+
+func replaceParamPlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":p", f.Param(), -1)
+}
+
+func replaceTagPlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":t", f.Tag(), -1)
+}
+
+func replaceActualTagPlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":at", f.ActualTag(), -1)
+}
+
+func replaceStructNamespacePlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":sn", f.Tag(), -1)
+}
+
+func replaceNamespacePlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":n", f.Namespace(), -1)
+}
+
+func replaceStructFieldPlaceholder(raw string, f validator.FieldError) string {
+	return strings.Replace(raw, ":sf", f.StructField(), -1)
+}
